f017d62feb0916b51ba2095949cec2fbeacf494b
Matias Paloranta <paloranta.matias@gmail.com>
2024-02-01 13:46:58 +0200

    Ex 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,22 +5,29 @@ const App = () => {
     {name: 'Arto Hellas'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    const person = {name: newName}
+    const person = {name: newName, number: newNumber}
     
     if (persons.some(item => areNamesEqual(person.name, item.name))) {
       alert(`${newName} is already added to the phonebook`)
     } else {
       setPersons(persons.concat(person))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
     //console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   const areNamesEqual = (name1, name2) => {
@@ -35,11 +42,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
