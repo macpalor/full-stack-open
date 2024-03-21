f1f0cfd8d4cdac8c2ed392cf0f0e707c8c5b8750
Matias Paloranta <paloranta.matias@gmail.com>
2024-03-21 12:52:25 +0200

    Ex 4.2: Refactor app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,48 +1,72 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
+
+// const blogSchema = new mongoose.Schema({
+//     title: String,
+//     author: String,
+//     url: String,
+//     likes: Number
+// })
+
+// const Blog = mongoose.model('Blog', blogSchema)
+
+// const mongoUrl = process.env.MONGODB_URI
+// //mongoose.set('strictQuery',false)
+
+// mongoose.connect(mongoUrl)
+//     .then(() =>{
+//         console.log(`Connected to ${mongoUrl}`)
+//     })
+//     .catch(error => {
+//         console.log('Error connecting to server:', error.message)
+//     })
+
+// app.use(cors())
+// app.use(express.json())
+
+// app.get('/api/blogs', (request, response) => {
+//     Blog
+//     .find({})
+//     .then(blogs => {
+//         response.json(blogs)
+//     })
+// })
+
+// app.post('/api/blogs', (request, response) => {
+//     const blog = new Blog(request.body)
+
+//     blog
+//     .save()
+//     .then(result => {
+//         response.status(201).json(result)
+//     })
+// })
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+mongoose.set('strictQuery', false)
 
-const Blog = mongoose.model('Blog', blogSchema)
+logger.info('connecting to', config.MONGODB_URI)
 
-const mongoUrl = process.env.MONGODB_URI
-//mongoose.set('strictQuery',false)
-
-mongoose.connect(mongoUrl)
-    .then(() =>{
-        console.log(`Connected to ${mongoUrl}`)
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
     })
     .catch(error => {
-        console.log('Error connecting to server:', error.message)
+        logger.error('error connecting to MongoDB:', error.message)
     })
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-    .find({})
-    .then(blogs => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-    .save()
-    .then(result => {
-        response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
