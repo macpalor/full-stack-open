c4ea563070968aa485d2b973df8b857f05954aaf
Matias Paloranta <paloranta.matias@gmail.com>
2024-03-21 13:07:08 +0200

    Finish Ex 4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,54 +1,9 @@
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
-// const cors = require('cors')
-// const mongoose = require('mongoose')
-
-// const blogSchema = new mongoose.Schema({
-//     title: String,
-//     author: String,
-//     url: String,
-//     likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-// const mongoUrl = process.env.MONGODB_URI
-// //mongoose.set('strictQuery',false)
-
-// mongoose.connect(mongoUrl)
-//     .then(() =>{
-//         console.log(`Connected to ${mongoUrl}`)
-//     })
-//     .catch(error => {
-//         console.log('Error connecting to server:', error.message)
-//     })
-
-// app.use(cors())
-// app.use(express.json())
-
-// app.get('/api/blogs', (request, response) => {
-//     Blog
-//     .find({})
-//     .then(blogs => {
-//         response.json(blogs)
-//     })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//     const blog = new Blog(request.body)
-
-//     blog
-//     .save()
-//     .then(result => {
-//         response.status(201).json(result)
-//     })
-// })
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -66,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
