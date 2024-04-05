fc0b036b29cd8b0820fb961dfa8f24532ed9a04e
Matias Paloranta <paloranta.matias@gmail.com>
2024-04-05 13:42:55 +0300

    Ex 4.15: implement tests

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -5,8 +5,10 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcryptjs')
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 describe('when there are initially some blogs saved', () => {
     beforeEach(async () => {
@@ -166,6 +168,39 @@ describe('when there are initially some blogs saved', () => {
         })
     })
     
+})
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash})
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'johndoe',
+            name: 'John Doe',
+            password: 'secret'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(user => user.username)
+        assert(usernames.includes(newUser.username))
+    })
 })
 
 after(async () => {
